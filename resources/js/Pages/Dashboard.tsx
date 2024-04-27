53a77e68d66e71f9c93390f10cdff624d377552e
t-miya2023 <t.miyata2023@gmail.com>
2024-04-27 09:29:53 +0900

    4/27

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -14,6 +14,7 @@ export default function Dashboard({ auth }: PageProps) {
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">マイページ</div>
+                        
                     </div>
                 </div>
             </div>
