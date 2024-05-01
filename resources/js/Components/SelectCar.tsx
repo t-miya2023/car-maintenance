c0781f63fce42fe0ae65e360389d46bdfac368fc
t-miya2023 <t.miyata2023@gmail.com>
2024-05-02 05:44:18 +0900

    5/2

--- resources/js/Components/SelectCar.tsx
+++ resources/js/Components/SelectCar.tsx
@@ -16,28 +16,32 @@ export const SelectCar = () => {
     const { cars } =usePage<PageProps>().props;
     // グローバルステートから取得
     const { selectCar, setSelectCar } = useContext(CarContext);
-    
+
+    const { control, setValue } = useForm();
+    // 初期値を入れる（配列の先頭の車種）
     useEffect(() => {
-        if(cars){
-            setSelectCar(cars[0].car_model);
+        if(cars && cars.length > 0){
+            const defalutCar = cars[0].car_model;
+            setSelectCar(defalutCar);
+            setValue('select',defalutCar);
+        }else{
+            setSelectCar('');  // 初期値を空文字列に設定して、常に制御された状態を保持
+            setValue('select', '選択可能な車種がありません');// 車種未登録の場合メッセージを表示
         }
-    }, [cars]);
+    }, [cars,setSelectCar,setValue]);
 
     console.log(selectCar);
-
-    const { control } = useForm();
 
     return (
         <Controller
         name="select"
         control={control}
-        defaultValue={selectCar}
         render={({ field }) => (
             <FormControl fullWidth >
                 <InputLabel>車種選択</InputLabel>
                 <Select
                     {...field}
-                    value={field.value}
+                    value={field.value || ''}
                     size="small"
                     sx={{backgroundColor:'white', width:'300px'}}
                     onChange={(event) => {
@@ -45,9 +49,13 @@ export const SelectCar = () => {
                         setSelectCar(event.target.value); // CarContext の値を更新
                     }}
                 >
-                    {cars.map(car => (
+                {cars && cars.length > 0 ? (
+                    cars.map(car => (
                         <MenuItem key={car.id} value={car.car_model || ''}>{car.car_model || '未登録'}</MenuItem>
-                    ))}
+                    ))
+                ) : (
+                    <MenuItem value='選択可能な車種がありません' disabled>選択可能な車種がありません</MenuItem>
+                )}
                 </Select>
             </FormControl>
         )}
