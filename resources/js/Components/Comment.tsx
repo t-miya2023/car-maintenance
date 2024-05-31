c9b546cffd0dfb3e713fd284c3a069186cf5d29c
t-miya2023 <t.miyata2023@gmail.com>
2024-06-01 05:01:25 +0900

    6/1

--- resources/js/Components/Comment.tsx
+++ resources/js/Components/Comment.tsx
@@ -1,9 +1,53 @@
-import { Box } from "@mui/material"
+import { CarContext } from "@/Providers/CarProvider";
+import { Cars } from "@/types/cars";
+import { Maintenaces } from "@/types/maintenaces";
+import { usePage } from "@inertiajs/react";
+import { Box, Typography } from "@mui/material"
+import React from "react";
 
-export const Comment = () => {
+type Props = {
+    currentYear: number;
+}
+interface PageProps {
+    cars: Cars[];
+    maintenances: Maintenaces[];
+    [key: string]: any;  // インデックスシグネチャを追加
+  }
+
+export const Comment = (props:Props) => {
+    const { currentYear } = props;
+      // Inertiaからデータを取得
+    const { cars, maintenances } =usePage<PageProps>().props;
+    // グローバルステートから取得
+    const { selectCar } = React.useContext(CarContext);
+    //selectCarから選択中の車のIDを取得
+    const currentCarId = cars.find(car => car.id === selectCar)?.id; 
+    const currentCarModel = cars.find(car => car.id === selectCar)?.car_model; 
+    // 選択中の車のメンテナンス情報を絞りこみ　空の可能性もあるためから配列を置く
+    const maintenanceArray = (maintenances || []).filter(maintenance => maintenance.car_id === currentCarId);
+    // 一年の合計金額を算出する
+    const yearSum = maintenanceArray.filter((array) => {
+        // 選択年のデータのみに絞る
+        const date = new Date(array.date);
+        const newDate = date.getFullYear();
+        return newDate === currentYear;
+    }).reduce((acc,array) => {
+        acc += Number(array.amount)
+        return acc;
+    },0)
+
     return (
-        <Box>
-            komennt
+        <Box sx={{
+            display:"flex",
+            alignItems: "center",
+            justifyContent: "center",
+            flexGrow: 1,
+            padding: "20px"
+        }}
+        >
+            <Typography fontSize={"2rem"}>
+                {currentYear}年の{currentCarModel}の維持費は{yearSum}円です
+            </Typography>
         </Box>
     )
 }
--- resources/js/Pages/Report.tsx
+++ resources/js/Pages/Report.tsx
@@ -26,7 +26,7 @@ export const Report = () => {
             </Grid>
             <Grid item xs={12}>
                 <Paper>
-                    <Comment />
+                    <Comment currentYear={currentYear} />
                 </Paper>
             </Grid>
             <Grid item xs={12} md={6}>
