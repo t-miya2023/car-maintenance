7fa68a662c38384ee0f254f18a7bea758d333a42
t-miya2023 <t.miyata2023@gmail.com>
2024-06-10 10:28:44 +0900

    6/10

--- resources/js/types/d3-scale.d.ts
+++ resources/js/types/d3-scale.d.ts
@@ -1,3 +1,12 @@
 declare module 'd3-scale' {
     export * from 'd3-scale';
+    export interface ScaleBand { /* ScaleBandの定義 */ }
+    export interface ScaleLogarithmic { /* ScaleLogarithmicの定義 */ }
+    export interface ScalePower { /* ScalePowerの定義 */ }
+    export interface ScaleTime { /* ScaleTimeの定義 */ }
+    export interface ScaleLinear { /* ScaleLinearの定義 */ }
+    export interface ScalePoint { /* ScalePointの定義 */ }
+    export interface ScaleOrdinal { /* ScaleOrdinalの定義 */ }
+    export interface ScaleSequential { /* ScaleSequentialの定義 */ }
+    export interface ScaleThreshold { /* ScaleThresholdの定義 */ }
 }
--- resources/js/types/d3-shape.d.ts
+++ resources/js/types/d3-shape.d.ts
@@ -1,3 +1,5 @@
 declare module 'd3-shape' {
     export * from 'd3-shape';
+    export interface Series { /* Seriesの定義 */ }
+    export interface PieArcDatum { /* PieArcDatumの定義 */ }
 }
